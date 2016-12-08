292ea98e0c2dd1b4f59fb7aafff9d422e9105f16
Nicolas Salas V <nikosalas@gmail.com>
2016-12-08 02:20:09 -0300

    Fix tests: gracefully fail when python dependencies are not satisfied

--- test/importmagic-test.el
+++ test/importmagic-test.el
@@ -11,9 +11,8 @@
 ;; Server available and importmagic can run on Python-mode buffers
 (ert-deftest importmagic-server-available ()
   (with-temp-buffer
-    (python-mode)
-    (importmagic-mode)
-    (should importmagic-server)))
+    (importmagic--testcase
+     (should importmagic-server))))
 
 
 ;; Let's define a bad buffer
@@ -31,12 +30,11 @@ future = datetime.timedelta(hours=1)
 (ert-deftest importmagic-unresolved-symbols ()
   (with-temp-buffer
     (insert importmagic-bad-buffer-test)
-    (python-mode)
-    (importmagic-mode)
-    (let ((expected-symbols '("os.path.join" "os.getcwd" "datetime.timedelta" "datetime.now"))
-          (actual-symbols (importmagic--get-unresolved-symbols)))
-      (dolist (symbol expected-symbols)
-        (should (member symbol actual-symbols))))))
+    (importmagic--testcase
+     (let ((expected-symbols '("os.path.join" "os.getcwd" "datetime.timedelta" "datetime.now"))
+           (actual-symbols (importmagic--get-unresolved-symbols)))
+       (dolist (symbol expected-symbols)
+         (should (member symbol actual-symbols)))))))
 
 ;; Every test from now on rebind the completing read function with
 ;; cl-letf. I really struggled with it. So thanks @Malabarba
@@ -45,48 +43,45 @@ future = datetime.timedelta(hours=1)
 ;; Test that importmagic-fix-imports ends up with a good buffer
 (ert-deftest importmagic-fix-imports-good ()
   (with-temp-buffer
-    (python-mode)
-    (importmagic-mode)
-    (progn
-      (insert importmagic-bad-buffer-test)
-      (cl-letf (((symbol-function 'completing-read)
-                 (lambda (prompt vals pred match initial history default)
-                   (cond
-                    ((string= "Querying for os.path.join: " prompt) "import os.path")
-                    ((string= "Querying for os.getcwd: " prompt) "import os")
-                    ((string= "Querying for datetime.timedelta: " prompt) "import datetime")
-                    ((string= "Querying for datetime.now: " prompt) "import datetime")
-                    (t nil) ; Fail otherwise
-                    ))))
-        (importmagic-fix-imports)
-        (dolist (required-symbol test/importmagic-bad-buffer-needs)
-          (should (s-contains? required-symbol (importmagic--buffer-as-string))))))))
+    (importmagic--testcase
+     (progn
+       (insert importmagic-bad-buffer-test)
+       (cl-letf (((symbol-function 'completing-read)
+                  (lambda (prompt vals pred match initial history default)
+                    (cond
+                     ((string= "Querying for os.path.join: " prompt) "import os.path")
+                     ((string= "Querying for os.getcwd: " prompt) "import os")
+                     ((string= "Querying for datetime.timedelta: " prompt) "import datetime")
+                     ((string= "Querying for datetime.now: " prompt) "import datetime")
+                     (t nil)          ; Fail otherwise
+                     ))))
+         (importmagic-fix-imports)
+         (dolist (required-symbol test/importmagic-bad-buffer-needs)
+           (should (s-contains? required-symbol (importmagic--buffer-as-string)))))))))
 
 ;; Test that a dummy buffer (with "import os") fixes the unresolved
 ;; symbol "os" by manually typing it.
 (ert-deftest importmagic-query-symbol-good ()
   (with-temp-buffer
-    (python-mode)
-    (importmagic-mode)
-    (insert "os.path")
-    (cl-letf (((symbol-function 'completing-read)
-               (lambda (prompt collection &optional predicate require-match initial history default inherit-input-method)
-                 "import os")))
-      (importmagic-fix-symbol "os")
-      (should (s-contains? "import os" (importmagic--buffer-as-string))))))
+    (importmagic--testcase
+     (insert "os.path")
+     (cl-letf (((symbol-function 'completing-read)
+                (lambda (prompt collection &optional predicate require-match initial history default inherit-input-method)
+                  "import os")))
+       (importmagic-fix-symbol "os")
+       (should (s-contains? "import os" (importmagic--buffer-as-string)))))))
 
 ;; Test symbol at point is ok.
 (ert-deftest importmagic-fix-at-point-good-2 ()
   (with-temp-buffer
-    (python-mode)
-    (importmagic-mode)
-    (insert "os.path")
-    (backward-char 6)
-    (cl-letf (((symbol-function 'completing-read)
-               (lambda (prompt collection &optional predicate require-match initial history default inherit-input-method)
-                 "import os")))
-      (importmagic-fix-symbol-at-point)
-      (should (s-contains? "import os" (importmagic--buffer-as-string))))))
+    (importmagic--testcase
+     (insert "os.path")
+     (backward-char 6)
+     (cl-letf (((symbol-function 'completing-read)
+                (lambda (prompt collection &optional predicate require-match initial history default inherit-input-method)
+                  "import os")))
+       (importmagic-fix-symbol-at-point)
+       (should (s-contains? "import os" (importmagic--buffer-as-string)))))))
 
 
 ;; Should fail on other major modes. Maybe add more for completeness?
@@ -113,26 +108,32 @@ future = datetime.timedelta(hours=1)
 ;; destroyed upon importmagic's deactivation
 (ert-deftest importmagic-epc-server-is-actually-destroyed ()
   (with-temp-buffer
-    (python-mode)
-    (importmagic-mode)
-    (should importmagic-server)
-    (importmagic-mode -1)
-    (should (not importmagic-server))))
+    (importmagic--testcase
+     (should importmagic-server)
+     (importmagic-mode -1)
+     (should (not importmagic-server)))))
 
 ;; Other buffers shouldn't inherit current buffer's EPC server
 (ert-deftest importmagic-epc-server-is-actually-buffer-local ()
   (let ((buffer1 (get-buffer-create "*A Python Buffer*"))
         (buffer2 (get-buffer-create "*Another Python buffer*")))
     (unwind-protect
-        (with-temp-buffer buffer1
-                          (python-mode)
-                          (importmagic-mode)
-                          (should importmagic-server)
-                          (with-temp-buffer buffer2
-                                            (python-mode)
-                                            (should (not importmagic-server))
-                                            (importmagic-mode)
-                                            (should importmagic-server))))))
+        (with-temp-buffer
+          buffer1
+          (importmagic--testcase
+           (should importmagic-server))
+          (with-temp-buffer
+            buffer2
+            (python-mode)
+            (should (not importmagic-server))
+            (condition-case nil
+                (progn
+                  (importmagic-mode)
+                  (should importmagic-server))
+              (error (progn
+                       (should (not importmagic-server))
+                       (should (not (symbol-value 'importmagic-mode)))
+                       (should (not (epc:live-p importmagic-server)))))))))))
 
 
 
--- test/test-helper.el
+++ test/test-helper.el
@@ -2,3 +2,17 @@
 
 (let ((impmagic-dir (f-dirname (f-dirname (f-this-file)))))
   (add-to-list 'load-path impmagic-dir))
+
+
+(defmacro importmagic--testcase (&rest body)
+  "Run BODY in python and importmagic mode."
+  (declare (indent 1))
+  `(progn
+     (python-mode)
+     (importmagic-mode)
+     (if (not importmagic-server)
+         (progn
+           (should (not importmagic-server)) ; duh
+           (should (not (symbol-value 'importmagic-mode)))
+           (should (not (epc:live-p importmagic-server))))
+       ,@body)))
